38d577f010e997ce9f9db000f665306420320bd2
cngabriel28 <cngabriel@loyola.edu>
2025-02-04 13:55:29 -0500

    finish the code from today's class

---
+++ lecture_note_4_5/dice/DiceTest.java
@@ -0,0 +1,12 @@
+package lecture_note_4_5.dice;
+//Test class
+
+import static java.awt.Color.green;
+
+public class DiceTest {
+    public static void main(String[] args) { //main function
+
+        Dice d6 = new Dice();
+        Dice d12 = new Dice("12 Side, "green", "12")
+    }
+}
